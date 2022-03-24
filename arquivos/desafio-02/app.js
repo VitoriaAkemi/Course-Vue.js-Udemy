a8ab666715311b8ef677ef88e6119b90944c20ff
VitoriaAkemi <vitoriaakemitanimura@gmail.com>
2022-03-24 16:03:28 -0300

    desafio2 concluido

--- arquivos/desafio-02/app.js
+++ arquivos/desafio-02/app.js
@@ -1,6 +1,17 @@
 new Vue({
     el: '#desafio',
+
     data: {
-        valor: ''
+        valor: 'Variavel valor '
+    },
+
+    methods: {
+        alerta(){
+            alert('Condicoes atendidas para gerar alert!!')
+        },
+        alteraValor(event){
+          this.valor = event.target.value
+        }
     }
+
 })
