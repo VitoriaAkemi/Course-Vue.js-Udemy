90686c2936a4ce59819cc3de9b363e8c051ad31a
VitoriaAkemi <vitoria.tanimura@telefonica.com>
2022-07-07 17:42:43 -0300

    add desafio-components-

---
+++ arquivos/componente-desafio-/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+      <TitleMenu/>
+      <v-divider></v-divider>
+      <Menu/>
+    </v-navigation-drawer>
+    <Header/>
+    <Carrossel/>
+    <Footer/>
+  </v-app>
+</template>
+
+<script>
+
+import Footer from './Footer.vue'
+import Carrossel from './Carrossel.vue'
+import Header from './Header.vue'
+import TitleMenu from './TitleMenu.vue'
+import Menu from './Menu.vue'
+
+
+export default {
+  components:{
+    Footer,
+    Carrossel,
+    Header,
+    TitleMenu,
+    Menu
+  },
+}
+</script>
