caeb88fa892b37e15fdd22de5050f0b935022a83
VitoriaAkemi <vitoria.tanimura@telefonica.com>
2022-07-07 16:16:30 -0300

    excluindo arquivos de vue cli

--- vue-cli/componentes/src/main.js
+++
@@ -1,9 +0,0 @@
-import App from './App.vue'
-import Contador from './Contador.vue'
-
-VueElement.config.productionTip = false
-VueElement.component('app-contador', Contador)
-
-new Vue({
-    render:h => h(App),
-}).$mount('#app')
